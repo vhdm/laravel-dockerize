3ce173ced5f7abad4adf2bf570de460724fab75c
Vahid <mahmoudian.vahid@gmail.com>
2021-05-13 20:33:27 +0430

    update installer.sh

--- installer.sh
+++ installer.sh
@@ -203,6 +203,7 @@ if ping -q -c 1 -W 1 google.com >/dev/null; then
   warning "docker-compose up ..."
   sleep 2
   sudo docker-compose --env-file "$LARAVEL_DIRECTORY"/.env up 
+  sudo docker exec vhdm_laravel_php_fpm composer install
 else
   error "The network is down or very slow!"
 fi
