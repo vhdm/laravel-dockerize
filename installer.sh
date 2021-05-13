1b04ec6bdb3f0b48a0bccd3df9d922aaf96b3368
Vahid <vhdm@users.noreply.github.com>
2021-05-13 16:29:29 +0430

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,14 +1,9 @@
-# The following function prints a text using custom color
-# -c or --color define the color for the print. See the array colors for the available options.
-# -n or --noline directs the system not to print a new line after the content.
-# Last argument is the message to be printed.
+#!/bash/bin
 RESET="\033[0m"
 BOLD="\033[1m"
 READ_NORMAL_COLOR="\033[38;5;31m"
 READ_ERROR_COLOR="\033[38;0;31m"
-
 cecho () {
- 
     declare -A colors;
     colors=(\
         ['black']='\E[0;47m'\
@@ -20,15 +15,12 @@ cecho () {
         ['cyan']='\E[0;36m'\
         ['white']='\E[0;37m'\
     );
- 
     local defaultMSG="No message passed.";
     local defaultColor="black";
     local defaultNewLine=true;
- 
     while [[ $# -gt 1 ]];
     do
     key="$1";
- 
     case $key in
         -c|--color)
             color="$2";
@@ -43,11 +35,9 @@ cecho () {
     esac
     shift;
     done
- 
     message=${1:-$defaultMSG};   # Defaults to default message.
     color=${color:-$defaultColor};   # Defaults to default color, if not specified.
     newLine=${newLine:-$defaultNewLine};
- 
     echo -en "${colors[$color]}";
     echo -en "$message";
     if [ "$newLine" = true ] ; then
